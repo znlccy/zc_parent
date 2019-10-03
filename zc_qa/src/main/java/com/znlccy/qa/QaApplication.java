ea5886f01d1a0850a4e98c73225335331bc32bb7
chencongye <756311868@qq.com>
2019-10-03 18:58:56 +0800

    提交初始代码 v1

---
+++ zc_qa/src/main/java/com/znlccy/qa/QaApplication.java
@@ -0,0 +1,18 @@
+package com.znlccy.qa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: znlccy
+ * @version: 1.0.0
+ * @date: 2019-10-03-18:44
+ * @remark: 问答启动类
+ */
+@SpringBootApplication
+public class QaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QaApplication.class, args);
+    }
+}
